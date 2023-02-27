ef7baf1b3d4e011dd832d5f3e3622cbb194db264
Gopi2312 <123471487+Gopi2312@users.noreply.github.com>
2023-02-27 12:01:12 +0530

    commit

--- src/main/java/assignment/tictactoa/Check.java
+++ src/main/java/assignment/tictactoa/Check.java
@@ -9,12 +9,25 @@ abstract class Check
 	PrintStream ps = new PrintStream(new FileOutputStream(FileDescriptor.out));
 	int size;
 	char[][] ch;
-    String str1 = "Player 1 Winner";
-    String str2 = "Player 2 Winner";
+	String str1 = "Player 1 Winner";
+	String str2 = "Player 2 Winner";
 	Check(int size,char[][] ch)
 	{
 		this.size = size;
 		this.ch = ch;
+	}
+	public void winner(int flag,int flag1)
+	{
+		if(flag == size)
+		{
+			ps.print(str1);
+			System.exit(0);
+		}
+		if(flag1 == size)
+		{
+			ps.print(str2);
+			System.exit(0);
+		}
 	}
     public abstract void check();
 }
